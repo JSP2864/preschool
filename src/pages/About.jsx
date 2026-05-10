28051678454a7b9d1f7c5bd4c0c2622d6bcf81c6
JSP <jsp@192.168.1.4>
2026-05-11 00:48:59 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,11 @@ export default function About() {
     <div className="about">
       <div className="relative mx-auto max-w-6xl px-4 pt-6 md:pt-8">
           <div className="news-marquee">
-            <div className="news-marquee__label">Latest</div>
+            <div className="news-marquee__label" aria-label="Latest">
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path d="M12 2l1.8 5.4L19 9l-5.2 1.6L12 16l-1.8-5.4L5 9l5.2-1.6L12 2zm6 11l.9 2.7 2.7.9-2.7.9-.9 2.7-.9-2.7-2.7-.9 2.7-.9.9-2.7zM5 14l.7 2.1 2.1.7-2.1.7L5 19.6l-.7-2.1-2.1-.7 2.1-.7L5 14z"/>
+              </svg>
+            </div>
             <div className="news-marquee__track" aria-label="Summer camp announcement">
               <div className="news-marquee__content">
                 <span>Summer Camp starts 18 May onward</span>
@@ -29,6 +33,11 @@ export default function About() {
             </div>
             <a href="tel:+918287839782" className="news-marquee__call">
               82878 39782
+            </a>
+            <a href="tel:+918287839782" className="news-marquee__call-mobile" aria-label="Call 82878 39782">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
+              </svg>
             </a>
           </div>
         </div>
